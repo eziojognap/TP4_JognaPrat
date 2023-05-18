eb2cafb67942dfc925450220d31d75f056948f8f
eziojognap <eziojognap@gmail.com>
2023-05-17 21:07:11 -0300

    Board support anda. Finalizado

--- src/main.c
+++ src/main.c
@@ -43,69 +43,10 @@
 #include "digital.h"
 #include "chip.h"
 #include <stdbool.h>
+#include "bsp.h"
 
 /* === Macros definitions
  * ====================================================================== */
-
-#define LED_R_PORT 2
-#define LED_R_PIN  0
-#define LED_R_FUNC SCU_MODE_FUNC4
-#define LED_R_GPIO 5
-#define LED_R_BIT  0
-
-#define LED_G_PORT 2
-#define LED_G_PIN  1
-#define LED_G_FUNC SCU_MODE_FUNC4
-#define LED_G_GPIO 5
-#define LED_G_BIT  1
-
-#define LED_B_PORT 2
-#define LED_B_PIN  2
-#define LED_B_FUNC SCU_MODE_FUNC4
-#define LED_B_GPIO 5
-#define LED_B_BIT  2
-
-#define LED_1_PORT 2
-#define LED_1_PIN  10
-#define LED_1_FUNC SCU_MODE_FUNC0
-#define LED_1_GPIO 0
-#define LED_1_BIT  14
-
-#define LED_2_PORT 2
-#define LED_2_PIN  11
-#define LED_2_FUNC SCU_MODE_FUNC0
-#define LED_2_GPIO 1
-#define LED_2_BIT  11
-
-#define LED_3_PORT 2
-#define LED_3_PIN  12
-#define LED_3_FUNC SCU_MODE_FUNC0
-#define LED_3_GPIO 1
-#define LED_3_BIT  12
-
-#define TEC_1_PORT 1
-#define TEC_1_PIN  0
-#define TEC_1_FUNC SCU_MODE_FUNC0
-#define TEC_1_GPIO 0
-#define TEC_1_BIT  4
-
-#define TEC_2_PORT 1
-#define TEC_2_PIN  1
-#define TEC_2_FUNC SCU_MODE_FUNC0
-#define TEC_2_GPIO 0
-#define TEC_2_BIT  8
-
-#define TEC_3_PORT 1
-#define TEC_3_PIN  2
-#define TEC_3_FUNC SCU_MODE_FUNC0
-#define TEC_3_GPIO 0
-#define TEC_3_BIT  9
-
-#define TEC_4_PORT 1
-#define TEC_4_PIN  6
-#define TEC_4_FUNC SCU_MODE_FUNC0
-#define TEC_4_GPIO 1
-#define TEC_4_BIT  9
 
 /* === Private data type declarations
  * ========================================================== */
@@ -130,79 +71,36 @@
 
 int main(void) {
 
-    digital_output_puntero led_verde;
-    digital_output_puntero led_rojo;
-    digital_output_puntero led_amarillo;
-    digital_output_puntero led_azul;
-
-    digital_input_puntero Boton1;
-    digital_input_puntero Boton2;
-    digital_input_puntero Boton3;
-    digital_input_puntero Boton4;
-
     int divisor = 0;
     bool current_state, last_state = false;
 
-    /********* LED RGB **********/
-    Chip_SCU_PinMuxSet(LED_R_PORT, LED_R_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_R_FUNC);
-    Chip_GPIO_SetPinState(LPC_GPIO_PORT, LED_R_GPIO, LED_R_BIT, false);
-    Chip_GPIO_SetPinDIR(LPC_GPIO_PORT, LED_R_GPIO, LED_R_BIT, true);
-
-    Chip_SCU_PinMuxSet(LED_G_PORT, LED_G_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_G_FUNC);
-    Chip_GPIO_SetPinState(LPC_GPIO_PORT, LED_G_GPIO, LED_G_BIT, false);
-    Chip_GPIO_SetPinDIR(LPC_GPIO_PORT, LED_G_GPIO, LED_G_BIT, true);
-
-    Chip_SCU_PinMuxSet(LED_B_PORT, LED_B_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_B_FUNC);
-    led_azul = DigitalOutputCreate(LED_B_GPIO, LED_B_BIT);
-
-    /********** LEDs 1 2 y 3 ********/
-    Chip_SCU_PinMuxSet(LED_1_PORT, LED_1_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_1_FUNC);
-    led_rojo = DigitalOutputCreate(LED_1_GPIO, LED_1_BIT);
-
-    Chip_SCU_PinMuxSet(LED_2_PORT, LED_2_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_2_FUNC);
-    led_amarillo = DigitalOutputCreate(LED_2_GPIO, LED_2_BIT);
-
-    Chip_SCU_PinMuxSet(LED_3_PORT, LED_3_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_3_FUNC);
-    led_verde = DigitalOutputCreate(LED_3_GPIO, LED_3_BIT);
-
-    /********* Teclas 1 2 3 y 4 *********/
-    Chip_SCU_PinMuxSet(TEC_1_PORT, TEC_1_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_1_FUNC);
-    Boton1 = DigitalInputCreate(TEC_1_GPIO, TEC_1_BIT, true);
-
-    Chip_SCU_PinMuxSet(TEC_2_PORT, TEC_2_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_2_FUNC);
-    Boton2 = DigitalInputCreate(TEC_2_GPIO, TEC_2_BIT, true);
-
-    Chip_SCU_PinMuxSet(TEC_3_PORT, TEC_3_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_3_FUNC);
-    Boton3 = DigitalInputCreate(TEC_3_GPIO, TEC_3_BIT, true);
-
-    Chip_SCU_PinMuxSet(TEC_4_PORT, TEC_4_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_4_FUNC);
-    Boton4 = DigitalInputCreate(TEC_4_GPIO, TEC_4_BIT, true);
+    board_puntero board = BoardCreate();
 
     /********** Lógica ************/
     while (true) {
-        if (true == DigitalInputGetState(Boton1)) {
-            DigitalOutputActivate(led_azul);
+        if (true == DigitalInputGetState(board->Boton1)) {
+            DigitalOutputActivate(board->led_azul);
         } else {
-            DigitalOutputDeactivate(led_azul);
+            DigitalOutputDeactivate(board->led_azul);
         }
 
-        current_state = (true == DigitalInputGetState(Boton2));
+        current_state = (true == DigitalInputGetState(board->Boton2));
         if ((current_state) && (!last_state)) {
-            DigitalOutputToggle(led_rojo);
+            DigitalOutputToggle(board->led_rojo);
         }
         last_state = current_state;
 
-        if (true == DigitalInputGetState(Boton3)) {
-            DigitalOutputActivate(led_amarillo);
+        if (true == DigitalInputGetState(board->Boton3)) {
+            DigitalOutputActivate(board->led_amarillo);
         }
-        if (true == DigitalInputGetState(Boton4)) {
-            DigitalOutputDeactivate(led_amarillo);
+        if (true == DigitalInputGetState(board->Boton4)) {
+            DigitalOutputDeactivate(board->led_amarillo);
         }
 
         divisor++;
         if (divisor == 2) {
             divisor = 0;
-            DigitalOutputToggle(led_verde);
+            DigitalOutputToggle(board->led_verde);
         }
 
         for (int index = 0; index < 100; index++) {
